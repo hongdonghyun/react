7cb10f731bbf523427370cd3513ab830f206a847
hongdonghyun <hong4367@gmail.com>
2017-12-11 16:53:28 +0900

    if const let

--- first/first-day/src/App.js
+++ first/first-day/src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 
 class App extends Component {
   render() { // 없으면 에러남
+    // var const let es6는 var안쓰임
+    // let 지역변수 const 전역변수
+    // if문대신 조건부 연산자
+    // [condition] ? [true] : [false]
     const name = 'hong3'
     return (
       
@@ -12,6 +16,7 @@ class App extends Component {
         <h1>hello wold</h1>
         <h2>enjoy react</h2>
         <p>{name}</p>
+        <p> { 1+1 ===2 ? 'true' : 'false'} </p>
         </div>
     );
   }
