2a58aa15c49c31e09d27967cb16cb4f1e8f58104
hongdonghyun <hong4367@gmail.com>
2017-12-16 17:33:41 +0900

    yarn add open-color

--- css-module-tutorial/src/App.js
+++ css-module-tutorial/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import style from './App.css';
-console.log(style)
-console.log(style.blueBox)
+import style from './App.scss';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(style)
+
 class App extends Component {
   render() {
     return (
       <div>
-        <div className={style.blueBox}></div>
-        <div className="redBox"></div>
+        <div className={cx('card','one')}></div>
+        <div className={cx('card','two')}></div>
+        <div className={cx('card','three')}></div>
+        <div className={cx('card','four')}></div>
+        
       </div>
     );
   }
